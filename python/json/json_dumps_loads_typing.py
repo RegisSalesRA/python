66a4b2b9ed38d15c2800f3d56e42f94689a92403
RegisSalesRA <regismsales@gmail.com>
2023-09-22 16:27:08 -0300

    big files

--- python/json/json_dumps_loads_typing.py
+++ python/json/json_dumps_loads_typing.py
@@ -1,34 +1,37 @@
+# json.dump e json.load com arquivos
 import json
-# from pprint import pprint
-from typing import TypedDict
+import os
+
+NOME_ARQUIVO = 'aula177.json'
+CAMINHO_ABSOLUTO_ARQUIVO = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        NOME_ARQUIVO
+    )
+)
+
+print(os.path.join(os.path.dirname(__file__), NOME_ARQUIVO))
 
 
-class Movie(TypedDict):
-    title: str
-    original_title: str
-    is_movie: bool
-    imdb_rating: float
-    year: int
-    characters: list[str]
-    budget: None | float
+filme = {
+    'title': 'O Senhor dos Anéis: A Sociedade do Anel',
+    'original_title': 'The Lord of the Rings: The Fellowship of the Ring',
 
-
-string_json = '''
-{
-  "title": "O Senhor dos Anéis: A Sociedade do Anel",
-  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
-  "is_movie": true,
-  "imdb_rating": 8.8,
-  "year": 2001,
-  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
-  "budget": null
+    'is_movie': True,
+    'imdb_rating': 8.8,
+    'year': 2001,
+    'characters': ['Frodo', 'Sam', 'Gandalf', 'Legolas', 'Boromir'],
+    'budget': None
 }
-'''
-filme: Movie = json.loads(string_json)
-# pprint(filme, width=40)
-# print(filme['title'])
-# print(filme['characters'][0])
-# print(filme['year'] + 10)
 
-json_string = json.dumps(filme, ensure_ascii=False, indent=2)
-print(json_string)
+
+
+with open(CAMINHO_ABSOLUTO_ARQUIVO, 'w') as arquivo:
+    json.dump(filme, arquivo, ensure_ascii=False, indent=2)
+
+
+
+with open(CAMINHO_ABSOLUTO_ARQUIVO, 'r') as arquivo:
+    filme_do_json = json.load(arquivo)
+    print(filme_do_json)
+ 
